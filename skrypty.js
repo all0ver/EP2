4e910335c6ba2a2494bbd443530228539ae34943
all0ver <60571521+all0ver@users.noreply.github.com>
2020-10-02 11:15:26 +0200

    Add files via upload

---
+++ skrypty.js
@@ -0,0 +1,17 @@
+var a = "10";
+var x = 10;
+var y = "hello";
+var z = true;
+
+document.getElementById("output").innerHTML =
+"a is " + typeof a + "<br>" +
+"x is " + typeof x + "<br>" +
+"y is " + typeof y + "<br>" +
+"z is " + typeof z;
+
+var a = 10;
+document.getElementById("output2").innerHTML =
+"a is " + typeof a + "<br>" +
+"x is " + typeof x + "<br>" +
+"y is " + typeof y + "<br>" +
+"z is " + typeof z;
